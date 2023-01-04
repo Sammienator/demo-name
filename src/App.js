df27507f7ce380c003a82ae287a246f5c929b999
Sammienator <sammugo18@gmail.com>
2023-01-04 23:39:57 +0300

    added text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        My app has been deployed here
+        My app has been deployed here and now i want to deploy 
       </header>
     </div>
   );
